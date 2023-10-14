c81aabf762abdc5fed77dbf4df38857f757f247b
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2023-10-14 23:58:31 +0200

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Hero from './components/Hero';
 import Demo from './components/Demo';
-jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
 import './App.css';
 const App = () => {
   return (
